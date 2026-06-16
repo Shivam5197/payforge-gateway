ba7fb20b2ba969457656bbe94ce9c63fff203d42
Shivam Sharma <73551425+Shivam5197@users.noreply.github.com>
2026-06-16 19:21:54 +0530

    Initial commit

--- src/main/java/com/payforge/gateway/PayForgeGatewayApplication.java
+++ src/main/java/com/payforge/gateway/PayForgeGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PayforgeGatewayApplication {
+public class PayForgeGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PayforgeGatewayApplication.class, args);
+		SpringApplication.run(PayForgeGatewayApplication.class, args);
 	}
 
 }
